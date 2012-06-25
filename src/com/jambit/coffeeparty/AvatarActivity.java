ecb6b7783a9c646c48499458c990b4c4633546ec
Benedikt <benedikt.meiler@jambit.com>
2012-06-25 21:46:01 +0200

    Added Camera in AvatarActivity

--- src/com/jambit/coffeeparty/AvatarActivity.java
+++ src/com/jambit/coffeeparty/AvatarActivity.java
@@ -2,9 +2,11 @@ package com.jambit.coffeeparty;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +15,7 @@ import android.widget.Gallery;
 import android.widget.TextView;
 
 public class AvatarActivity extends Activity {
+    private static final int TAKE_PHOTO_ACTIONCODE = 0;
     public static String PLAYERNAME_EXTRA = "playerName";
     public static String SELECTED_AVATAR_EXTRA = "selected_avatar";
     private Intent data = new Intent();
@@ -47,6 +50,18 @@ public class AvatarActivity extends Activity {
     }
 
     public void cameraButtonOnClick(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, TAKE_PHOTO_ACTIONCODE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Bundle extras = data.getExtras();
+        if (requestCode == TAKE_PHOTO_ACTIONCODE) {
+            Bitmap mImageBitmap = (Bitmap) extras.get("data");
+            data.putExtra(SELECTED_AVATAR_EXTRA, mImageBitmap);
+            imageAdapter.addBitmap(mImageBitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- src/com/jambit/coffeeparty/ImageAdapter.java
+++ src/com/jambit/coffeeparty/ImageAdapter.java
@@ -1,23 +1,29 @@
 package com.jambit.coffeeparty;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.Gallery;
 import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
     int mGalleryItemBackground;
     private Context mContext;
 
-    private Integer[] mImageIds = { R.drawable.droid_green, R.drawable.droid_red, R.drawable.droid_blue,
-            R.drawable.droid_yellow };
+    private List<Bitmap> bitmaps = new ArrayList<Bitmap>();
 
     public ImageAdapter(Context c) {
         mContext = c;
-
+        bitmaps.add(((BitmapDrawable) mContext.getResources().getDrawable(R.drawable.droid_green)).getBitmap());
+        bitmaps.add(((BitmapDrawable) mContext.getResources().getDrawable(R.drawable.droid_red)).getBitmap());
+        bitmaps.add(((BitmapDrawable) mContext.getResources().getDrawable(R.drawable.droid_blue)).getBitmap());
+        bitmaps.add(((BitmapDrawable) mContext.getResources().getDrawable(R.drawable.droid_yellow)).getBitmap());
         TypedArray a = c.obtainStyledAttributes(R.styleable.Gallery1);
         mGalleryItemBackground = a.getResourceId(R.styleable.Gallery1_android_galleryItemBackground, 0);
         a.recycle();
@@ -25,29 +31,42 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return mImageIds.length;
+        return bitmaps.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return mContext.getResources().getDrawable(mImageIds[position]);
+        return bitmaps.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return mImageIds[position];
+        // TODO what to return here when Bitmaps are stored
+        assert (false);
+        return 0;
+    }
+
+    public void addBitmap(Bitmap bitmap) {
+        bitmaps.add(bitmap);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView i = new ImageView(mContext);
+        // ImageView i = new ImageView(mContext);
+        //
+        // i.setImageResource(mResImageIds[position]);
+        // i.setLayoutParams(new Gallery.LayoutParams(150, 100));
+        // i.setScaleType(ImageView.ScaleType.FIT_XY);
+        // i.setBackgroundResource(mGalleryItemBackground);
+        //
+        // return i;
 
-        i.setImageResource(mImageIds[position]);
-        i.setLayoutParams(new Gallery.LayoutParams(150, 100));
-        i.setScaleType(ImageView.ScaleType.FIT_XY);
-        i.setBackgroundResource(mGalleryItemBackground);
+        if (convertView == null) {
+            convertView = new ImageView(mContext);
+        }
+        ((ImageView) convertView).setImageBitmap(bitmaps.get(position));
+        return convertView;
 
-        return i;
     }
 
 }
