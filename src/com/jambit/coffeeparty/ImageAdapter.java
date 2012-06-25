d47e059b914e8fcc2fae0c8a342283819d11b533
Benedikt <benedikt.meiler@jambit.com>
2012-06-25 17:58:01 +0200

    Added AvatarActivity.    "  ImageAdapter.

---
+++ src/com/jambit/coffeeparty/ImageAdapter.java
@@ -0,0 +1,56 @@
+package com.jambit.coffeeparty;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+    int mGalleryItemBackground;
+    private Context mContext;
+
+    private Integer[] mImageIds = { R.drawable.droid_green, R.drawable.droid_red, R.drawable.droid_blue,
+            R.drawable.droid_yellow };
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+
+        TypedArray a = c.obtainStyledAttributes(R.styleable.Gallery1);
+        mGalleryItemBackground = a.getResourceId(R.styleable.Gallery1_android_galleryItemBackground, 0);
+        a.recycle();
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        assert (false); // Not implemented
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return mImageIds[position];
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView i = new ImageView(mContext);
+
+        i.setImageResource(mImageIds[position]);
+        i.setLayoutParams(new Gallery.LayoutParams(150, 100));
+        i.setScaleType(ImageView.ScaleType.FIT_XY);
+        i.setBackgroundResource(mGalleryItemBackground);
+
+        return i;
+    }
+
+}
--- src/com/jambit/coffeeparty/MainMenuActivity.java
+++ src/com/jambit/coffeeparty/MainMenuActivity.java
@@ -7,26 +7,33 @@ import android.util.Log;
 import android.view.View;
 
 public class MainMenuActivity extends Activity {
-    
+
     private final static int NUM_PLAYERS_SET = 0;
-    
+    private final static int SET_AVATAR = 1;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
-    
-    public void onStartNewGame(View v){
+
+    public void onStartNewGame(View v) {
         Intent intent = new Intent(this, NumberOfPlayersActivity.class);
         startActivityForResult(intent, NUM_PLAYERS_SET);
+
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == NUM_PLAYERS_SET) {
             int numPlayers = data.getIntExtra("numPlayers", 2);
             Log.d("mainMenu", "Num Players set to " + numPlayers);
+            Intent avatarIntent = new Intent(this, AvatarActivity.class);
+            avatarIntent.putExtra(AvatarActivity.PLAYERNAME_EXTRA, "TestPlayer");
+            startActivityForResult(avatarIntent, SET_AVATAR);
+        } else if (requestCode == SET_AVATAR) {
+            Log.i("mainMenu", "AvatarActivity returned.");
         }
 
         super.onActivityResult(requestCode, resultCode, data);
