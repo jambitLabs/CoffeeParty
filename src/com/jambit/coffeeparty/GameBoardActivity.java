d807b1f3492f012822e04bb156b3a4c7d43a7d41
Sebi <sebastian.vogel@jambit.com>
2012-06-26 20:33:01 +0200

    Added player names to the game board figures

--- src/com/jambit/coffeeparty/GameBoardActivity.java
+++ src/com/jambit/coffeeparty/GameBoardActivity.java
@@ -9,6 +9,7 @@ import org.anddev.andengine.engine.camera.Camera;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.Entity;
 import org.anddev.andengine.entity.IEntity;
 import org.anddev.andengine.entity.modifier.PathModifier;
 import org.anddev.andengine.entity.modifier.PathModifier.IPathModifierListener;
@@ -17,7 +18,9 @@ import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.SpriteBackground;
 import org.anddev.andengine.entity.sprite.AnimatedSprite;
 import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.text.Text;
 import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.font.Font;
 import org.anddev.andengine.opengl.texture.TextureOptions;
 import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
@@ -26,6 +29,8 @@ import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 import org.anddev.andengine.ui.activity.BaseGameActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.Display;
 import android.view.MotionEvent;
 
@@ -37,20 +42,32 @@ import com.jambit.coffeeparty.model.Player;
 public class GameBoardActivity extends BaseGameActivity {
 
     private final static int DICE_ROLLED = 110;
-    
+
     private boolean mDiceEnabled = false;
-    
+
     private BitmapTextureAtlas bitmapTextureAtlas;
     private TextureRegion backgroundTexture;
     private TiledTextureRegion playerSpriteTexture;
 
-    private class PlayerSprite extends AnimatedSprite {
+    private Font playerNameFont;
+
+    private class PlayerSprite extends Entity {
         private final Player player;
 
+        private AnimatedSprite figure;
+        private Text figureNameText;
+
         PlayerSprite(Player player, int x, int y) {
-            super(x, y, playerSpriteTexture);
+            super(x, y);
             this.player = player;
-            animate(new Random().nextInt(10) + 100);
+
+            this.figure = new AnimatedSprite(-playerSpriteTexture.getWidth() / 2, -playerSpriteTexture.getHeight() / 2,
+                    playerSpriteTexture);
+            this.figure.animate(new Random().nextInt(10) + 100);
+            this.attachChild(figure);
+
+            this.figureNameText = new Text(0, -20, playerNameFont, player.getName());
+            this.attachChild(figureNameText);
         }
 
         public Player getPlayer() {
@@ -63,40 +80,40 @@ public class GameBoardActivity extends BaseGameActivity {
     public void movePlayer(Player player, int oldPosition, int newPosition) {
         Map gameMap = getGame().getMap();
         PlayerSprite playerSprite = getPlayerSpriteForPlayer(player);
-        
+
         if (oldPosition == newPosition) {
             Field field = gameMap.getFieldForPosition(newPosition);
             playerSprite.setPosition(field.getX(), field.getY());
-        }
-        else
-        {
+        } else {
             int wayPoints = newPosition - oldPosition + 1;
             Path path = new Path(wayPoints);
-            
-            for (int position = oldPosition; position <= newPosition; position++)
-            {
+
+            for (int position = oldPosition; position <= newPosition; position++) {
                 Field field = gameMap.getFieldForPosition(position);
                 path.to(field.getX(), field.getY());
             }
-            
-            playerSprite.registerEntityModifier(new PathModifier(wayPoints * 1.0f, path, null, new IPathModifierListener() {
-                @Override
-                public void onPathStarted(final PathModifier pPathModifier, final IEntity pEntity) {
-                }
 
-                @Override
-                public void onPathWaypointStarted(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
-                }
+            playerSprite.registerEntityModifier(new PathModifier(wayPoints * 1.0f, path, null,
+                    new IPathModifierListener() {
+                        @Override
+                        public void onPathStarted(final PathModifier pPathModifier, final IEntity pEntity) {
+                        }
 
-                @Override
-                public void onPathWaypointFinished(final PathModifier pPathModifier, final IEntity pEntity, final int pWaypointIndex) {
-                }
+                        @Override
+                        public void onPathWaypointStarted(final PathModifier pPathModifier, final IEntity pEntity,
+                                final int pWaypointIndex) {
+                        }
 
-                @Override
-                public void onPathFinished(final PathModifier pPathModifier, final IEntity pEntity) {
-                    // @Alex: This is for you! :)
-                }
-            }));
+                        @Override
+                        public void onPathWaypointFinished(final PathModifier pPathModifier, final IEntity pEntity,
+                                final int pWaypointIndex) {
+                        }
+
+                        @Override
+                        public void onPathFinished(final PathModifier pPathModifier, final IEntity pEntity) {
+                            // @Alex: This is for you! :)
+                        }
+                    }));
         }
     }
 
@@ -115,7 +132,7 @@ public class GameBoardActivity extends BaseGameActivity {
     @Override
     public void onLoadResources() {
         String boardImage = getGame().getMap().getBoardImage();
-                
+
         this.bitmapTextureAtlas = new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR);
         this.backgroundTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.bitmapTextureAtlas, this,
                 boardImage, 0, 0);
@@ -123,6 +140,13 @@ public class GameBoardActivity extends BaseGameActivity {
                 this, "face_box_tiled.png", 132, 180, 2, 1);
 
         this.mEngine.getTextureManager().loadTexture(this.bitmapTextureAtlas);
+
+        BitmapTextureAtlas fontTexture = new BitmapTextureAtlas(256, 256, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        this.playerNameFont = new Font(fontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 20, true,
+                Color.BLACK);
+
+        this.mEngine.getTextureManager().loadTexture(fontTexture);
+        this.mEngine.getFontManager().loadFont(this.playerNameFont);
     }
 
     @Override
@@ -130,25 +154,23 @@ public class GameBoardActivity extends BaseGameActivity {
         reset();
 
         this.mEngine.registerUpdateHandler(new FPSLogger());
-        
+
         final Scene scene = new Scene();
         // scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
         scene.setBackground(new SpriteBackground(new Sprite(0, 0, backgroundTexture)));
 
         createPlayers(scene);
         placePlayers();
-           
+
         mDiceEnabled = true;
         return scene;
     }
 
-    private void reset()
-    {
+    private void reset() {
         playerSprites.clear();
     }
-    
-    private Game getGame()
-    {
+
+    private Game getGame() {
         return ((CoffeePartyApplication) getApplication()).getGameState();
     }
 
@@ -157,7 +179,7 @@ public class GameBoardActivity extends BaseGameActivity {
 
         for (Player player : gameState.getPlayers()) {
             Field fieldOfPlayer = gameState.getMap().getFieldOfPlayer(player);
-            
+
             PlayerSprite playerSprite = new PlayerSprite(player, fieldOfPlayer.getX(), fieldOfPlayer.getY());
             playerSprites.add(playerSprite);
             scene.attachChild(playerSprite);
@@ -184,8 +206,7 @@ public class GameBoardActivity extends BaseGameActivity {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_UP && mDiceEnabled)
-        {
+        if (event.getAction() == MotionEvent.ACTION_UP && mDiceEnabled) {
             mDiceEnabled = false;
             Intent intent = new Intent(this, DiceRollActivity.class);
             startActivityForResult(intent, DICE_ROLLED);
@@ -193,10 +214,10 @@ public class GameBoardActivity extends BaseGameActivity {
         }
         return false;
     }
-    
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == DICE_ROLLED){
+        if (requestCode == DICE_ROLLED) {
             int diceResult = data.getExtras().getInt(getString(R.string.dice_result));
             Game gameState = getGame();
             Player currentPlayer = gameState.getCurrentPlayer();
@@ -205,7 +226,7 @@ public class GameBoardActivity extends BaseGameActivity {
             int newPosition = currentPlayer.getPosition();
             movePlayer(currentPlayer, oldPosition, newPosition);
             gameState.nextRound();
-            //TODO: move this to after minigame finished
+            // TODO: move this to after minigame finished
             mDiceEnabled = true;
         }
     }
