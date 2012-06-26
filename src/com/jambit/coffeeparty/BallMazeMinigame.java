acc741ed1adb0936b829f7940de075a684424def
Benedikt <benedikt.meiler@jambit.com>
2012-06-26 16:24:25 +0200

    Added BallMaze minigame (stub only). Resized Camera in MinigameBaseActivity.

---
+++ src/com/jambit/coffeeparty/BallMazeMinigame.java
@@ -0,0 +1,48 @@
+package com.jambit.coffeeparty;
+
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+
+public class BallMazeMinigame extends MinigameBaseActivity {
+
+    private BitmapTextureAtlas bitmapTextureAtlas;
+    private TextureRegion backgroundTexture;
+    private TiledTextureRegion playerSpriteTexture;
+
+    public Scene onLoadScene() {
+        final Scene scene = super.onLoadScene();
+
+        AnimatedSprite test = new AnimatedSprite(30, 80, playerSpriteTexture);
+        scene.attachChild(test);
+
+        return scene;
+    }
+
+    @Override
+    public void onLoadResources() {
+        super.onLoadResources();
+        this.bitmapTextureAtlas = new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR);
+
+        this.backgroundTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.bitmapTextureAtlas,
+                                                                                        this,
+                                                                                        "gameboard.png",
+                                                                                        0,
+                                                                                        0);
+
+        this.playerSpriteTexture = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.bitmapTextureAtlas,
+                                                                                               this,
+                                                                                               "",
+                                                                                               132,
+                                                                                               180,
+                                                                                               2,
+                                                                                               1);
+
+        this.mEngine.getTextureManager().loadTexture(this.bitmapTextureAtlas);
+
+    }
+}
