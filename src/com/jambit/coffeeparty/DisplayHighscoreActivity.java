2f881cc8e179c2233119152c2aae293a417be852
Sebastian Cohausz <sebastian.cohausz@partner.bmw.de>
2012-06-28 12:22:13 +0200

    Add method for registering game results as highscore

--- src/com/jambit/coffeeparty/DisplayHighscoreActivity.java
+++ src/com/jambit/coffeeparty/DisplayHighscoreActivity.java
@@ -1,12 +1,10 @@
 package com.jambit.coffeeparty;
 
-import com.jambit.coffeeparty.db.HighscoreDBHelper;
 import com.jambit.coffeeparty.db.HighscoreDataSource;
 
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.SimpleCursorAdapter;
 
 public class DisplayHighscoreActivity extends ListActivity {
 	HighscoreDataSource dataSource;
@@ -16,12 +14,18 @@ public class DisplayHighscoreActivity extends ListActivity {
 		dataSource = new HighscoreDataSource(this);
 		dataSource.openForReading();
 		Cursor c = dataSource.getAllHighscores();
-		String[] mapFrom = {HighscoreDBHelper.COLUMN_USERNAME, HighscoreDBHelper.COLUMN_SCORE, HighscoreDBHelper.COLUMN_TIMESTAMP};
-		int[] mapTo = {R.id.highscore_nameview, R.id.highscore_scoreview, R.id.highscore_dateview};
 		
 		HighscoreCursorAdapter adapter = new HighscoreCursorAdapter(this, c);
 		
 		this.setListAdapter(adapter);
 		this.getListView().setClickable(false);
+		this.getListView().setFocusable(false);
+	}
+	
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		dataSource.close();
 	}
 }
