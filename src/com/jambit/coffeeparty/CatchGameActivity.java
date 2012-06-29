7ac0d0935cad5528ff5a148e735b0885d5b2dc09
Sebi <sebastian.vogel@jambit.com>
2012-06-29 11:44:16 +0200

    Improving the catch-game

--- src/com/jambit/coffeeparty/CatchGameActivity.java
+++ src/com/jambit/coffeeparty/CatchGameActivity.java
@@ -9,6 +9,12 @@ import org.anddev.andengine.engine.handler.timer.TimerHandler;
 import org.anddev.andengine.entity.modifier.MoveModifier;
 import org.anddev.andengine.entity.modifier.PathModifier;
 import org.anddev.andengine.entity.modifier.PathModifier.Path;
+import org.anddev.andengine.entity.particle.ParticleSystem;
+import org.anddev.andengine.entity.particle.emitter.PointParticleEmitter;
+import org.anddev.andengine.entity.particle.initializer.GravityInitializer;
+import org.anddev.andengine.entity.particle.initializer.VelocityInitializer;
+import org.anddev.andengine.entity.particle.modifier.AlphaModifier;
+import org.anddev.andengine.entity.particle.modifier.ExpireModifier;
 import org.anddev.andengine.entity.scene.Scene;
 import org.anddev.andengine.entity.scene.background.SpriteBackground;
 import org.anddev.andengine.entity.sprite.AnimatedSprite;
@@ -27,6 +33,7 @@ public class CatchGameActivity extends MinigameBaseActivity {
 
     private TextureRegion theThingToCatchTexture;
     private TextureRegion backgroundTexture;
+    private TextureRegion beanTexture;
     private TiledTextureRegion meadowTexture;
 
     private Sprite thingToCatchSprite;
@@ -70,6 +77,11 @@ public class CatchGameActivity extends MinigameBaseActivity {
                 "huml.png", 0, 0);
         mEngine.getTextureManager().loadTexture(bitmapTextureAtlas);
 
+        bitmapTextureAtlas = new BitmapTextureAtlas(128, 128, TextureOptions.BILINEAR);
+        beanTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(bitmapTextureAtlas, this,
+                "jambitbean_white.png", 0, 0);
+        mEngine.getTextureManager().loadTexture(bitmapTextureAtlas);
+
         bitmapTextureAtlas = new BitmapTextureAtlas(1024, 1024, TextureOptions.BILINEAR);
         backgroundTexture = BitmapTextureAtlasTextureRegionFactory.createFromAsset(bitmapTextureAtlas, this,
                 "cloud.png", 0, 0);
@@ -95,7 +107,7 @@ public class CatchGameActivity extends MinigameBaseActivity {
             public void onUpdate(float pSecondsElapsed) {
                 long now = System.currentTimeMillis();
                 
-                if (now - startTime > 60 * 1000)
+                if (now - startTime > 30 * 1000)
                 {
                     onGameFinished();
                 } else
@@ -156,14 +168,78 @@ public class CatchGameActivity extends MinigameBaseActivity {
         if (touchX >= thingToCatchSprite.getX() && touchX < thingToCatchSprite.getX() + thingToCatchSprite.getWidth()
                 && touchY >= thingToCatchSprite.getY()
                 && touchY < thingToCatchSprite.getY() + thingToCatchSprite.getHeight()) {
+            showParticles(event.getX(), event.getY());
             catchTheThing();
-            jumpToNewLocation();
+//            jumpToNewLocation();
             updateScoreDisplay();
 
             return true;
         } else {
             return false;
         }
+    }
+
+    private ParticleSystem createParticleSystem (float particlesXSpawn, float particlesYSpawn, final TextureRegion particleTextureRegion) {
+
+        //Max & min rate are the maximum particles per second and the minimum particles per second.
+        final float maxRate = 40;
+        final float minRate = 20;
+
+        //This variable determines the maximum particles in the particle system.
+        final int maxParticles = 150;
+
+        //Particle emitter which will set all of the particles at a ertain point when they are initialized.
+        final PointParticleEmitter pointParticleEmtitter = new PointParticleEmitter(particlesXSpawn, particlesYSpawn);
+
+        //Creating the particle system.
+        final ParticleSystem particleSystem = new ParticleSystem(pointParticleEmtitter, maxRate, minRate, maxParticles, particleTextureRegion);
+
+        //And now, lets create the initiallizers and modifiers.
+        //Velocity initiallizer - will pick a random velocity from -20 to 20 on the x & y axes. Play around with this value.
+        particleSystem.addParticleInitializer(new VelocityInitializer(-100, 100, -100, 100));
+
+        //Acceleration initializer - gives all the particles the earth gravity (so they accelerate down).
+        GravityInitializer gravity = new GravityInitializer();
+        gravity.setAcceleration(0, 0, 980f, 980f);
+        particleSystem.addParticleInitializer(gravity);
+
+        //And now, adding an alpha modifier, so particles slowly fade out. This makes a particle go from alpha = 1 to alpha = 0 in 3 seconds, starting exactly when the particle is spawned.
+        particleSystem.addParticleModifier(new AlphaModifier(1, 0, 0.5f, 1.5f));
+
+        //Lastly, expire modifier. Make particles die after 3 seconds - their alpha reached 0.
+        particleSystem.addParticleModifier(new ExpireModifier(1.5f));
+
+        return particleSystem;
+
+    }
+
+    private void showParticles(float x, float y) {
+        final ParticleSystem particleSystem = createParticleSystem(x, y, beanTexture);
+        
+        final Scene scene = mEngine.getScene();
+        
+        if (particleSystem.hasParent()) {
+            particleSystem.detachSelf();
+        }
+        particleSystem.setPosition(x, y);
+        
+        scene.attachChild(particleSystem);
+        
+        scene.registerUpdateHandler(new TimerHandler(0.25f, new ITimerCallback() {
+            @Override
+            public void onTimePassed(TimerHandler pTimerHandler) {
+                particleSystem.setParticlesSpawnEnabled(false);
+                scene.unregisterUpdateHandler(pTimerHandler);
+            }
+        }));
+
+        scene.registerUpdateHandler(new TimerHandler(3f, new ITimerCallback() {
+            @Override
+            public void onTimePassed(TimerHandler pTimerHandler) {
+                scene.detachChild(particleSystem);
+                scene.unregisterUpdateHandler(pTimerHandler);
+            }
+        }));
     }
 
     @Override
